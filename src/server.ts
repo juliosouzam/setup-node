0b373d2e999881064187527892754e4ee2080c54
Julio Cesar <julio.souzam@gmail.com>
2020-07-16 01:10:29 -0300

    babel

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,5 @@ const app = express();
 
 app.get('/', (request, response) => response.json({ status: true }));
 
+// eslint-disable-next-line no-console
 app.listen(3333, () => console.log('running'));
